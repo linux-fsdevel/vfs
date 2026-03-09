2d070a17e32e7db436e5daa41dc8dc81419c192d
Ojaswin Mujoo <ojaswin@linux.ibm.com>
2026-03-09 23:04:31 +0530

    iomap: Support buffered RWF_WRITETHROUGH via async dio backend
    This adds initial support for performing buffered RWF_WRITETHROUGH write.
    The rough flow for a writethrough write is as follows:
    
    1. Acquire inode lock and call iomap begin to get an allocated mapping.
    2. Acquire folio lock.
    3. Perform a memcpy from user buffer to the folio and mark it dirty
    4. Wait for any current writeback to complete and then call folio_mkclean()
       to prevent mmap writes from changing it.
    5. Start writeback on the folio
    6. Use dio codepath to send an asynchronous dio. We use the
       inode_dio_begin/end() logic for writethrough as well to serialize
       against paths like truncate.
    7. Once the IO is queued, the write syscall is free to unlock folio and
       return.
    8. In the endio path, cleanup resources, record any errors and clear
       writeback on folio.
    
    Few things to note in the design:
    
    1. Folio handling note: We might be writing through a partial folio so
    we need to be careful to not clear the folio dirty bit unless there are
    no dirty blocks in the folio after the writethrough.
    
    2. we call iomap_begin() twice, one at the start and the other is within
    the iomap_dio_rw(). Functionally this should be okay since the second
    call should just return whatever the first call allocated. This
    redundancy is kept as a tradeoff to avoid churning too much code for
    the initial PoC.
    
    3. Along with the writeback bit, we also use inode_dio_begin() to
    synchronize against paths like truncate. This might be too restrictive
    but we can look into this in next revisions.
    
    4. Freezing support is a WIP. Check the comment on top of
    iomap_writethrough_iter()
    
    Suggested-by: Jan Kara <jack@suse.cz>
    Suggested-by: Dave Chinner <dgc@kernel.org>
    Signed-off-by: Ojaswin Mujoo <ojaswin@linux.ibm.com>

--- include/linux/iomap.h
+++ include/linux/iomap.h
@@ -202,6 +202,7 @@ struct iomap_write_ops {
 #endif /* CONFIG_FS_DAX */
 #define IOMAP_ATOMIC		(1 << 9) /* torn-write protection */
 #define IOMAP_DONTCACHE		(1 << 10)
+#define IOMAP_WRITETHROUGH	(1 << 11)
 
 struct iomap_ops {
 	/*
@@ -468,6 +469,15 @@ struct iomap_writepage_ctx {
 	void			*wb_ctx;	/* pending writeback context */
 };
 
+struct iomap_writethrough_ctx {
+	struct kiocb iocb;
+	struct folio *folio;
+	struct inode *inode;
+	struct bio_vec *bvec;
+	loff_t orig_pos;
+	loff_t orig_len;
+};
+
 struct iomap_ioend *iomap_init_ioend(struct inode *inode, struct bio *bio,
 		loff_t file_offset, u16 ioend_flags);
 struct iomap_ioend *iomap_split_ioend(struct iomap_ioend *ioend,
@@ -575,6 +585,14 @@ struct iomap_dio_ops {
  */
 #define IOMAP_DIO_BOUNCE		(1 << 4)
 
+/*
+ * Set when we are using the dio path to perform writethrough for
+ * RWF_WRITETHROUGH buffered write. The ->endio handler must check this
+ * to perform any writethrough related cleanup like ending writeback on
+ * a folio.
+ */
+#define IOMAP_DIO_BUF_WRITETHROUGH	(1 << 5)
+
 ssize_t iomap_dio_rw(struct kiocb *iocb, struct iov_iter *iter,
 		const struct iomap_ops *ops, const struct iomap_dio_ops *dops,
 		unsigned int dio_flags, void *private, size_t done_before);
@@ -583,6 +601,20 @@ struct iomap_dio *__iomap_dio_rw(struct kiocb *iocb, struct iov_iter *iter,
 		unsigned int dio_flags, void *private, size_t done_before);
 ssize_t iomap_dio_complete(struct iomap_dio *dio);
 void iomap_dio_bio_end_io(struct bio *bio);
+
+/*
+ * In writethrough, we copy user data to folio first and then send the folio
+ * to writeback via dio path. To achieve this, we need callbacks from iomap_ops,
+ * iomap_write_ops and iomap_dio_ops. This struct packs them together.
+ */
+struct iomap_writethrough_ops {
+	const struct iomap_ops *ops;
+	const struct iomap_write_ops *write_ops;
+	const struct iomap_dio_ops *dio_ops;
+};
+ssize_t iomap_file_writethrough_write(struct kiocb *iocb, struct iov_iter *i,
+				      const struct iomap_writethrough_ops *wt_ops,
+				      void *private);
 
 #ifdef CONFIG_SWAP
 struct file;
