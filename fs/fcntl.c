d9643c6944a078d3124daeba89526d4d93648196
Jori Koolstra <jkoolstra@xs4all.nl>
2026-03-10 21:44:33 +0100

    vfs: add support for empty path to openat2(2)
    To get an operable version of an O_PATH file descriptor, it is possible
    to use openat(fd, ".", O_DIRECTORY) for directories, but other files
    currently require going through open("/proc/<pid>/fd/<nr>"), which
    depends on a functioning procfs.
    
    This patch adds the OPENAT2_EMPTY_PATH flag to openat2(2). If passed,
    LOOKUP_EMPTY is set at path resolve time.
    
    Note: This implies that you cannot rely anymore on disabling procfs from
    being mounted (e.g. inside a container without procfs mounted and with
    CAP_SYS_ADMIN dropped) to prevent O_PATH fds from being re-opened
    read-write.
    
    Signed-off-by: Jori Koolstra <jkoolstra@xs4all.nl>

--- fs/fcntl.c
+++ fs/fcntl.c
@@ -1169,8 +1169,8 @@ static int __init fcntl_init(void)
 	 * Exceptions: O_NONBLOCK is a two bit define on parisc; O_NDELAY
 	 * is defined as O_NONBLOCK on some platforms and not on others.
 	 */
-	BUILD_BUG_ON(20 - 1 /* for O_RDONLY being 0 */ !=
-		HWEIGHT32(
+	BUILD_BUG_ON(21 - 1 /* for O_RDONLY being 0 */ !=
+		HWEIGHT64(
 			(VALID_OPEN_FLAGS & ~(O_NONBLOCK | O_NDELAY)) |
 			__FMODE_EXEC));
 
