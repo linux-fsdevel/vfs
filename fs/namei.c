eda99d233250363edd43186af8bae0c89c1e4402
Jori Koolstra <jkoolstra@xs4all.nl>
2026-03-12 12:28:22 +0100

    vfs: fix docstring of hash_name()
    The docstring of hash_name() is falsely reporting that it returns the
    component length, whereas it returns a pointer to the terminating '/'
    or NUL character in the pathname being resolved.
    
    Signed-off-by: Jori Koolstra <jkoolstra@xs4all.nl>

--- fs/namei.c
+++ fs/namei.c
@@ -2437,8 +2437,13 @@ inside:
 EXPORT_SYMBOL(hashlen_string);
 
 /*
- * Calculate the length and hash of the path component, and
- * return the length as the result.
+ * hash_name - Calculate the length and hash of the path component
+ * @nd: the path resolution state
+ * @name: the pathname to read the component from
+ * @lastword: if the component fits in a single word, the component bytes,
+ * otherwise 0
+ *
+ * Returns: a pointer to the terminating '/' or NUL character in @name.
  */
 static inline const char *hash_name(struct nameidata *nd,
 				    const char *name,
