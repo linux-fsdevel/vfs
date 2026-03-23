c261a60a4c7fa98a0a01129b7ce4153d21f10a78
Andy Shevchenko <andriy.shevchenko@linux.intel.com>
2026-03-23 15:54:20 +0100

    initramfs: Refactor to use hex2bin() instead of custom approach
    There is a simple_strntoul() function used solely as a shortcut
    for hex2bin() with proper endianess conversions. Replace that
    and drop the unneeded function in the next changes.
    
    This implementation will abort if we fail to parse the cpio header,
    instead of using potentially bogus header values.
    
    Co-developed-by: David Disseldorp <ddiss@suse.de>
    Signed-off-by: David Disseldorp <ddiss@suse.de>
    Signed-off-by: Andy Shevchenko <andriy.shevchenko@linux.intel.com>

--- init/initramfs_test.c
+++ init/initramfs_test.c
@@ -499,8 +499,7 @@ static void __init initramfs_test_hdr_hex(struct kunit *test)
 {
 	char *err, *fmt;
 	size_t len;
-	struct kstat st0, st1;
-	char fdata[] = "this file data will be unpacked";
+	char fdata[] = "this file data will not be unpacked";
 	struct initramfs_test_bufs {
 		char cpio_src[(CPIO_HDRLEN + PATH_MAX + 3 + sizeof(fdata)) * 2];
 	} *tbufs = kzalloc(sizeof(struct initramfs_test_bufs), GFP_KERNEL);
@@ -528,28 +527,14 @@ static void __init initramfs_test_hdr_hex(struct kunit *test)
 	/*
 	 * override CPIO_HDR_FMT and instead use a format string which places
 	 * "0x" prefixes on the uid, gid and namesize values.
-	 * parse_header()/simple_str[n]toul() accept this.
+	 * parse_header()/simple_str[n]toul() accepted this, contrary to the
+	 * initramfs specification. hex2bin() now fails.
 	 */
 	fmt = "%s%08x%08x0x%06x0X%06x%08x%08x%08x%08x%08x%08x%08x0x%06x%08x%s";
 	len = fill_cpio(c, ARRAY_SIZE(c), fmt, tbufs->cpio_src);
 
 	err = unpack_to_rootfs(tbufs->cpio_src, len);
-	KUNIT_EXPECT_NULL(test, err);
-
-	KUNIT_EXPECT_EQ(test, init_stat(c[0].fname, &st0, 0), 0);
-	KUNIT_EXPECT_EQ(test, init_stat(c[1].fname, &st1, 0), 0);
-
-	KUNIT_EXPECT_TRUE(test,
-		uid_eq(st0.uid, make_kuid(current_user_ns(), (uid_t)0x123456)));
-	KUNIT_EXPECT_TRUE(test,
-		gid_eq(st0.gid, make_kgid(current_user_ns(), (gid_t)0x123457)));
-	KUNIT_EXPECT_TRUE(test,
-		uid_eq(st1.uid, make_kuid(current_user_ns(), (uid_t)0x56)));
-	KUNIT_EXPECT_TRUE(test,
-		gid_eq(st1.gid, make_kgid(current_user_ns(), (gid_t)0x57)));
-
-	KUNIT_EXPECT_EQ(test, init_unlink(c[0].fname), 0);
-	KUNIT_EXPECT_EQ(test, init_rmdir(c[1].fname), 0);
+	KUNIT_EXPECT_NOT_NULL(test, err);
 
 	kfree(tbufs);
 }
