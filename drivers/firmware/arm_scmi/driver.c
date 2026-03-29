ca3dbe3844462bc3890d25baa6ac1601d74bdb5c
Cristian Marussi <cristian.marussi@arm.com>
2026-03-29 17:33:19 +0100

    firmware: arm_scmi: Add basic Telemetry support
    Add SCMIv4.0 Telemetry basic support to enable initialization and resources
    enumeration: add all the telemetry messages definitions and parsing logic
    but only a few simple state gathering protocol operations.
    
    Signed-off-by: Cristian Marussi <cristian.marussi@arm.com>

--- drivers/firmware/arm_scmi/driver.c
+++ drivers/firmware/arm_scmi/driver.c
@@ -3508,6 +3508,7 @@ static int __init scmi_driver_init(void)
 	scmi_system_register();
 	scmi_powercap_register();
 	scmi_pinctrl_register();
+	scmi_telemetry_register();
 
 	return platform_driver_register(&scmi_driver);
 }
@@ -3526,6 +3527,7 @@ static void __exit scmi_driver_exit(void)
 	scmi_system_unregister();
 	scmi_powercap_unregister();
 	scmi_pinctrl_unregister();
+	scmi_telemetry_unregister();
 
 	platform_driver_unregister(&scmi_driver);
 
