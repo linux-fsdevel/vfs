e85d152afbf5b285f463ba3ebd3972d50403447b
Askar Safin <safinaskar@gmail.com>
2026-04-06 09:08:15 +0000

    vfs: init/main.c: change comment: opening /dev/console may fail
    This patch is for VFS.
    
    Today comment on console_on_rootfs says that opening /dev/console never
    fails. This is false. The opening may fail if /dev/console is not
    present in internal initramfs. So let's update the comment.
    
    I tried to fix the problem itself in [1], but the patchset was rejected,
    so I will fix the comment instead. :)
    
    Link: https://lore.kernel.org/all/20260219210312.3468980-1-safinaskar@gmail.com/ # [1]
    Signed-off-by: Askar Safin <safinaskar@gmail.com>

--- init/main.c
+++ init/main.c
@@ -1645,7 +1645,10 @@ static int __ref kernel_init(void *unused)
 	      "See Linux Documentation/admin-guide/init.rst for guidance.");
 }
 
-/* Open /dev/console, for stdin/stdout/stderr, this should never fail */
+/*
+ * Open /dev/console, for stdin/stdout/stderr, this may fail if you supplied
+ * your own internal initramfs without /dev/console
+ */
 void __init console_on_rootfs(void)
 {
 	struct file *file = filp_open("/dev/console", O_RDWR, 0);
