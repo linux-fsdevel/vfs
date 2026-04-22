766347f71c51357c92d17293738f2f3ed623fff8
Thorsten Blum <thorsten.blum@linux.dev>
2026-04-22 14:30:03 +0200

    namei: use QSTR() instead of QSTR_INIT() in path_pts
    Drop the hard-coded length argument and use the simpler QSTR().
    
    Signed-off-by: Thorsten Blum <thorsten.blum@linux.dev>
    Reviewed-by: Jan Kara <jack@suse.cz>

--- fs/namei.c
+++ fs/namei.c
@@ -3586,7 +3586,7 @@ int path_pts(struct path *path)
 	 */
 	struct dentry *parent = dget_parent(path->dentry);
 	struct dentry *child;
-	struct qstr this = QSTR_INIT("pts", 3);
+	struct qstr this = QSTR("pts");
 
 	if (unlikely(!path_connected(path->mnt, parent))) {
 		dput(parent);
