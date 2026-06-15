0873f1a10a5a29b6be00d5fd916a667722d365d5
Vincent Mailhol <mailhol@kernel.org>
2026-06-15 18:09:06 +0200

    riscv: define DPS root partition type UUIDs
    DPS [1] assigns GPT partition type UUIDs to operating system partitions.
    Root partitions use architecture-specific type UUIDs so the OS can
    discover the intended root filesystem without relying on a root= cmdline
    option.
    
    Define DPS_ROOT_PARTITION_TYPE_UUID in asm/dps_root.h for the RISC-V
    32/64-bit variants described by the specification and select
    ARCH_HAS_DPS_ROOT_PARTITION_TYPE_UUID.
    
    [1] The Discoverable Partitions Specification (DPS)
    Link: https://uapi-group.org/specifications/specs/discoverable_partitions_specification/
    
    Cc: Paul Walmsley <pjw@kernel.org>
    Cc: Palmer Dabbelt <palmer@dabbelt.com>
    Cc: Albert Ou <aou@eecs.berkeley.edu>
    Cc: linux-riscv@lists.infradead.org
    Signed-off-by: Vincent Mailhol <mailhol@kernel.org>

---
+++ arch/riscv/include/asm/dps_root.h
@@ -0,0 +1,12 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+
+#ifndef _ASM_RISCV_DPS_ROOT_H
+#define _ASM_RISCV_DPS_ROOT_H
+
+#ifdef CONFIG_64BIT
+#define DPS_ROOT_PARTITION_TYPE_UUID "72ec70a6-cf74-40e6-bd49-4bda08e8f224"
+#else
+#define DPS_ROOT_PARTITION_TYPE_UUID "60d5a7fe-8e7d-435c-b714-3dd8162144e1"
+#endif
+
+#endif /* _ASM_RISCV_DPS_ROOT_H */
