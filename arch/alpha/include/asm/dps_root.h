d9583d36e89221e3272443374f2591f3afdcabc8
Vincent Mailhol <mailhol@kernel.org>
2026-06-15 18:08:58 +0200

    alpha: define DPS root partition type UUID
    DPS [1] assigns GPT partition type UUIDs to operating system partitions.
    Root partitions use architecture-specific type UUIDs so the OS can
    discover the intended root filesystem without relying on a root= cmdline
    option.
    
    Define DPS_ROOT_PARTITION_TYPE_UUID in asm/dps_root.h for alpha and
    select ARCH_HAS_DPS_ROOT_PARTITION_TYPE_UUID.
    
    [1] The Discoverable Partitions Specification (DPS)
    Link: https://uapi-group.org/specifications/specs/discoverable_partitions_specification/
    
    Cc: Richard Henderson <richard.henderson@linaro.org>
    Cc: Matt Turner <mattst88@gmail.com>
    Cc: Magnus Lindholm <linmag7@gmail.com>
    Cc: linux-alpha@vger.kernel.org
    Signed-off-by: Vincent Mailhol <mailhol@kernel.org>

---
+++ arch/alpha/include/asm/dps_root.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+
+#ifndef _ASM_ALPHA_DPS_ROOT_H
+#define _ASM_ALPHA_DPS_ROOT_H
+
+#define DPS_ROOT_PARTITION_TYPE_UUID "6523f8ae-3eb1-4e2a-a05a-18b695ae656f"
+
+#endif /* _ASM_ALPHA_DPS_ROOT_H */
