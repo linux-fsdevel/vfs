3e1f552cfdf34c64a369bdc1ec740f5af2f22c58
Vincent Mailhol <mailhol@kernel.org>
2026-06-15 18:09:04 +0200

    parisc: define DPS root partition type UUID
    DPS [1] assigns GPT partition type UUIDs to operating system partitions.
    Root partitions use architecture-specific type UUIDs so the OS can
    discover the intended root filesystem without relying on a root= cmdline
    option.
    
    Define DPS_ROOT_PARTITION_TYPE_UUID in asm/dps_root.h for parisc and
    select ARCH_HAS_DPS_ROOT_PARTITION_TYPE_UUID.
    
    [1] The Discoverable Partitions Specification (DPS)
    Link: https://uapi-group.org/specifications/specs/discoverable_partitions_specification/
    
    Cc: James E.J. Bottomley <James.Bottomley@HansenPartnership.com>
    Cc: Helge Deller <deller@gmx.de>
    Cc: linux-parisc@vger.kernel.org
    Signed-off-by: Vincent Mailhol <mailhol@kernel.org>

---
+++ arch/parisc/include/asm/dps_root.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+
+#ifndef _ASM_PARISC_DPS_ROOT_H
+#define _ASM_PARISC_DPS_ROOT_H
+
+#define DPS_ROOT_PARTITION_TYPE_UUID "1aacdb3b-5444-4138-bd9e-e5c2239b2346"
+
+#endif /* _ASM_PARISC_DPS_ROOT_H */
