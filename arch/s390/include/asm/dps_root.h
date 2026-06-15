9524882e57e40b6be2164fe892de30a32d2ac5cf
Vincent Mailhol <mailhol@kernel.org>
2026-06-15 18:09:07 +0200

    s390: define DPS root partition type UUIDs
    DPS [1] assigns GPT partition type UUIDs to operating system partitions.
    Root partitions use architecture-specific type UUIDs so the OS can
    discover the intended root filesystem without relying on a root= cmdline
    option.
    
    Define DPS_ROOT_PARTITION_TYPE_UUID in asm/dps_root.h for the s390
    (32-bit) and s390x (64-bit) variants described by the specification and
    select ARCH_HAS_DPS_ROOT_PARTITION_TYPE_UUID.
    
    [1] The Discoverable Partitions Specification (DPS)
    Link: https://uapi-group.org/specifications/specs/discoverable_partitions_specification/
    
    Cc: Heiko Carstens <hca@linux.ibm.com>
    Cc: Vasily Gorbik <gor@linux.ibm.com>
    Cc: Alexander Gordeev <agordeev@linux.ibm.com>
    Cc: linux-s390@vger.kernel.org
    Signed-off-by: Vincent Mailhol <mailhol@kernel.org>

---
+++ arch/s390/include/asm/dps_root.h
@@ -0,0 +1,12 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+
+#ifndef _ASM_S390_DPS_ROOT_H
+#define _ASM_S390_DPS_ROOT_H
+
+#ifdef CONFIG_64BIT
+#define DPS_ROOT_PARTITION_TYPE_UUID "5eead9a9-fe09-4a1e-a1d7-520d00531306"
+#else
+#define DPS_ROOT_PARTITION_TYPE_UUID "08a7acea-624c-4a20-91e8-6e0fa67d23f9"
+#endif
+
+#endif /* _ASM_S390_DPS_ROOT_H */
