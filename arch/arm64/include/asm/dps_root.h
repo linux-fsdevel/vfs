96860d577662da0b25133db2744393bfacf56fdc
Vincent Mailhol <mailhol@kernel.org>
2026-06-15 18:09:01 +0200

    arm64: define DPS root partition type UUID
    DPS [1] assigns GPT partition type UUIDs to operating system partitions.
    Root partitions use architecture-specific type UUIDs so the OS can
    discover the intended root filesystem without relying on a root= cmdline
    option.
    
    Define DPS_ROOT_PARTITION_TYPE_UUID in asm/dps_root.h for arm64 and select
    ARCH_HAS_DPS_ROOT_PARTITION_TYPE_UUID.
    
    [1] The Discoverable Partitions Specification (DPS)
    Link: https://uapi-group.org/specifications/specs/discoverable_partitions_specification/
    
    Cc: Catalin Marinas <catalin.marinas@arm.com>
    Cc: Will Deacon <will@kernel.org>
    Cc: linux-arm-kernel@lists.infradead.org
    Signed-off-by: Vincent Mailhol <mailhol@kernel.org>

---
+++ arch/arm64/include/asm/dps_root.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+
+#ifndef _ASM_ARM64_DPS_ROOT_H
+#define _ASM_ARM64_DPS_ROOT_H
+
+#define DPS_ROOT_PARTITION_TYPE_UUID "b921b045-1df0-41c3-af44-4c6f280d3fae"
+
+#endif /* _ASM_ARM64_DPS_ROOT_H */
