767b1ccdefaa8706216d790a186b652b7baf6a93
Eric Biggers <ebiggers@kernel.org>
2026-06-18 18:48:52 +0000

    fscrypt: Use lock guards for mutexes
    Replace all remaining calls to mutex_lock() and mutex_unlock() in
    fs/crypto/ with lock guards.  No functional change.
    
    Signed-off-by: Eric Biggers <ebiggers@kernel.org>

--- fs/crypto/keyring.c
+++ fs/crypto/keyring.c
@@ -520,7 +520,7 @@ static int do_add_master_key(struct super_block *sb,
 	struct fscrypt_master_key *mk;
 	int err;
 
-	mutex_lock(&fscrypt_add_key_mutex); /* serialize find + link */
+	guard(mutex)(&fscrypt_add_key_mutex); /* serialize find + link */
 
 	mk = fscrypt_find_master_key(sb, mk_spec);
 	if (!mk) {
@@ -547,7 +547,6 @@ static int do_add_master_key(struct super_block *sb,
 		}
 		fscrypt_put_master_key(mk);
 	}
-	mutex_unlock(&fscrypt_add_key_mutex);
 	return err;
 }
 
