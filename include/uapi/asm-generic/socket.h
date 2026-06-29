51491f329fa1aefe4d0d548e18dfdf520f97a172
Jori Koolstra <jkoolstra@xs4all.nl>
2026-06-29 21:43:25 +0200

    net: af_unix: useful handling of LSM denials on SCM_RIGHTS
    Right now if some LSM such as Smack denies an AF_UNIX socket peer to
    receive an SCM_RIGHTS fd, the SCM_RIGHTS fd array will be cut short at
    that point, and MSG_CTRUNC is set on return of recvmsg(). This is
    highly problematic behaviour, because it leaves the receiver
    wondering what happened. As per man page MSG_CTRUNC is supposed to
    indicate that the control buffer was sized too short, but suddenly
    a permission error might result in the exact same flag being set.
    Moreover, the receiver has no chance to determine how many fds got
    originally sent and how many were suppressed.[1]
    
    Add a SO_RIGHTS_NOTRUNC option to UNIX sockets to enable more useful
    handling of LSM denials when receiving SCM_RIGHTS messages: instead of
    truncating the message at the first blocked fd, keep every fd slot
    and store the LSM errno in the blocked slot.
    
    [1]: https://github.com/uapi-group/kernel-features#useful-handling-of-lsm-denials-on-scm_rights
    
    Signed-off-by: Jori Koolstra <jkoolstra@xs4all.nl>

--- include/uapi/asm-generic/socket.h
+++ include/uapi/asm-generic/socket.h
@@ -150,6 +150,9 @@
 #define SO_INQ			84
 #define SCM_INQ			SO_INQ
 
+#define SO_RIGHTS_NOTRUNC	85
+#define SCM_RIGHTS_NOTRUNC	SO_RIGHTS_NOTRUNC
+
 #if !defined(__KERNEL__)
 
 #if __BITS_PER_LONG == 64 || (defined(__x86_64__) && defined(__ILP32__))
