3bd472d55669e76eeb681b4fde55f0118803b54e
Yang Xiuwei <yangxiuwei@kylinos.cn>
2026-08-06 16:50:40 +0800

    btrfs: don't stash uring encoded data across -EAGAIN
    Returning -EAGAIN while leaving btrfs_uring_encoded_data in the cmd PDU
    leaks if the request is cancelled or the ring exits before reissue.
    io_uring does not free driver PDU allocations on cleanup.
    
    Write: io_queue_sqe() always issues with IO_URING_F_NONBLOCK first, so
    return -EAGAIN before allocating and free data on every exit.
    
    Read: free on nowait -EAGAIN too; only -EIOCBQUEUED keeps the
    allocation for btrfs_uring_read_finished().
    
    Fixes: 34310c442e17 ("btrfs: add io_uring command for encoded reads (ENCODED_READ ioctl)")
    Fixes: e32dcdb0af9f ("btrfs: add io_uring interface for encoded writes")
    Signed-off-by: Yang Xiuwei <yangxiuwei@kylinos.cn>

--- fs/btrfs/ioctl.c
+++ fs/btrfs/ioctl.c
@@ -4783,7 +4783,7 @@ static int btrfs_uring_encoded_read(struct io_uring_cmd *cmd, unsigned int issue
 	ret = btrfs_encoded_read(&kiocb, &data->iter, &data->args, &cached_state,
 				 &disk_bytenr, &disk_io_size);
 	if (ret == -EAGAIN)
-		goto out_acct;
+		goto out_free;
 	if (ret < 0 && ret != -EIOCBQUEUED)
 		goto out_free;
 
@@ -4825,8 +4825,10 @@ out_acct:
 		add_rchar(current, ret);
 	inc_syscr(current);
 
-	if (ret != -EIOCBQUEUED && ret != -EAGAIN)
+	if (ret != -EIOCBQUEUED) {
 		kfree(data);
+		bc->data = NULL;
+	}
 
 	return ret;
 }
@@ -4852,6 +4854,11 @@ static int btrfs_uring_encoded_write(struct io_uring_cmd *cmd, unsigned int issu
 
 	if (!(file->f_mode & FMODE_WRITE)) {
 		ret = -EBADF;
+		goto out_acct;
+	}
+
+	if (issue_flags & IO_URING_F_NONBLOCK) {
+		ret = -EAGAIN;
 		goto out_acct;
 	}
 
@@ -4924,11 +4931,6 @@ static int btrfs_uring_encoded_write(struct io_uring_cmd *cmd, unsigned int issu
 		}
 	}
 
-	if (issue_flags & IO_URING_F_NONBLOCK) {
-		ret = -EAGAIN;
-		goto out_acct;
-	}
-
 	pos = data->args.offset;
 	ret = rw_verify_area(WRITE, file, &pos, data->args.len);
 	if (ret < 0)
@@ -4954,8 +4956,8 @@ out_acct:
 		add_wchar(current, ret);
 	inc_syscw(current);
 
-	if (ret != -EAGAIN)
-		kfree(data);
+	kfree(data);
+	bc->data = NULL;
 	return ret;
 }
 
